146a571875481e77cd4c6877f53988093d4fe567
ultraBoJack <1391534203@qq.com>
2017-09-02 11:55:34 +0800

    Update jsop.php

--- jsop.php
+++ jsop.php
@@ -1,5 +1,7 @@
-<?php  
-$arr=array('a'=>1,'b'=>2,'c'=>3,'d'=>4,'e'=>5);  
-$result=json_encode($arr);  
-$callback=$_GET['callback'];  
-echo $callback."($result)";  
+<?php
+header('Content-type: application/json');
+$arr=array('a'=>1,'b'=>2,'c'=>3,'d'=>4,'e'=>5);
+$result=json_encode($arr);
+$callback=$_GET['callback'];
+echo $callback."($result)";
+?>
