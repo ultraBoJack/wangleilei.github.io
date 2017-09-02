59e0d2f0a47b3f215a6b6c54023e1df00b122398
ultraBoJack <1391534203@qq.com>
2017-09-02 12:46:20 +0800

    Delete jsop.php

--- jsop.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-type: application/json');
-$arr=array('a'=>1,'b'=>2,'c'=>3,'d'=>4,'e'=>5);
-$result=json_encode($arr);
-$callback=$_GET['callback'];
-echo $callback."($result)";
-?>
